68d14bf11bf7411cb285a667fcc456ef74b621be
sgurnz <luca@epicuriosi.org>
2017-08-22 22:33:45 +0200

    Update create-cicd.sh
    Improved so that it waits for gogs to be up before configuring it and cloning the repo

--- create-cicd.sh
+++ create-cicd.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
-oc new-project stage --display-name="Shopping - Stage"
-oc new-project prod --display-name="Shopping - Prod"
-oc new-project cicd --display-name="CI/CD"
-
-
-oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n stage
-oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n prod
 
 #Should not be needed
 #oc policy add-role-to-user admin system:serviceaccount:cicd:default
 
-oc new-app -f http://bit.ly/openshift-gogs-persistent-template --param=HOSTNAME=gogs-cicd.apps.rhpds.openshift.opentlc.com -n cicd
-
+# CI/CD creation steps
+oc new-project cicd --display-name="CI/CD"
+oc new-app -f http://bit.ly/openshift-gogs-persistent-template --param=HOSTNAME=gogs-cicd.cloud.redhat.int -n cicd
 oc new-app jenkins-ephemeral -l app=jenkins -p MEMORY_LIMIT=1Gi -n cicd
-
 oc create -f ./pipelines.yaml  -n cicd
-
-oc create -f ./shopping-bluegreen.yaml  -n prod
-
 oc create -f  https://raw.githubusercontent.com/OpenShiftDemos/nexus/master/nexus2-persistent-template.yaml -n cicd
-
 oc new-app nexus2-persistent -n cicd
 
+# Stage Project creation steps
+oc new-project stage --display-name="Shopping - Stage"
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n stage
+
+# Prod Project creation steps
+oc new-project prod --display-name="Shopping - Prod"
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n prod
+oc create -f ./shopping-bluegreen.yaml  -n prod
 oc new-app shopping-bluegreen -l app=shopping -n prod
 
 #Optional
-oc new-app docker.io/openshiftdemos/sonarqube:6.0 \
--e SONARQUBE_JDBC_USERNAME=sonar,SONARQUBE_JDBC_PASSWORD=sonar,SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar -n cicd
+oc new-app docker.io/openshiftdemos/sonarqube:6.0 -e SONARQUBE_JDBC_USERNAME=sonar,SONARQUBE_JDBC_PASSWORD=sonar,SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar -n cicd
 oc expose service sonarqube -n cicd
 
-curl -sL --post302 -w "%{http_code}"  \ http://gogs-cicd.apps.rhpds.openshift.opentlc.com/user/sign_up --form user_name=gogs --form email=admin@gogs.com --form password=password --form retype=password
+# now we wait for gogs to be ready
+x=1
+oc get ep gogs -n cicd -o yaml | grep "\- addresses:"
+while [ ! $? -eq 0 ]
+do
+  sleep 10
+  x=$(( $x + 1 ))
+  if [ $x -gt 100 ]
+  then
+    exit 255
+  fi
+  oc get ep gogs -n cicd -o yaml | grep "\- addresses:"
+done
 
-curl -sL -w "%{http_code}" -H "Content-Type: application/json" -u gogs:password -X POST http://gogs-cicd.apps.rhpds.openshift.opentlc.com/api/v1/repos/migrate -d @./clone_repo.json
+# we might catch the router before it's been updated, so wait just a touch more
+sleep 10
+
+curl -sL --post302 -w "%{http_code}"  http://gogs-cicd.cloud.redhat.int/user/sign_up -d user_name=gogs -d email=admin@gogs.com -d password=password -d retype=password
+
+curl -sL -w "%{http_code}" -H "Content-Type: application/json" -u gogs:password -X POST http://gogs-cicd.cloud.redhat.int/api/v1/repos/migrate -d @./clone_repo.json
